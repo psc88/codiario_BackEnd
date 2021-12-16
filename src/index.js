0696fec2c8e63390a4e462f465ca869c20f870d6
Pablo Castillo <psc88sdrd@gmail.com>
2021-12-16 01:33:18 -0300

    se agrego nodemon

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'),()=>{
     console.log('estoy en el puerto '+ app.get('port'));
 })
+
+app.get('/', (req, res) => {
+     res.send('hola desde el servidor backend')  
+})
+
+app.delete('/borrardatos', (req, res) => {
+     res.send('aqui vamos a borrar')  
+})
+
