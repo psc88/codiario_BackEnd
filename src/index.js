31156390009027dc213bdfad8a10b61fc2aca7bf
Pablo Castillo <psc88sdrd@gmail.com>
2021-12-16 01:53:57 -0300

    se agrego morgan y cors con su configuracion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
@@ -8,10 +10,17 @@ app.listen(app.get('port'),()=>{
     console.log('estoy en el puerto '+ app.get('port'));
 })
 
+//midlewares
+app.use(morgan('dev'));
+app.use(cors());
+//permite recibir objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// rutas
 app.get('/', (req, res) => {
      res.send('hola desde el servidor backend')  
 })
-
 app.delete('/borrardatos', (req, res) => {
      res.send('aqui vamos a borrar')  
 })
