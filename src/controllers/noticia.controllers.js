6c6903dc2d66d40af616c0889d639eea2ff72103
Pablo Castillo <psc88sdrd@gmail.com>
2021-12-22 00:29:05 -0300

    se creo el metodo crear producto en mongo

--- src/controllers/noticia.controllers.js
+++ src/controllers/noticia.controllers.js
@@ -1,17 +1,39 @@
+import Noticia from "../models/noticia";
+
 const noticiaCtrl = {};
 
 // logica para obtener la lista de noticias
 noticiaCtrl.listarNoticias = (req, res) => {
-        res.send('aqui listo las noticias')
-    }
+    res.send('aqui listo las noticias')
+}
 
 noticiaCtrl.borrarProducto = (req, res) => {
     res.send('aqui borro las noticias')
 }
 
-noticiaCtrl.crearNoticia = (req, res) => {
-    console.log(req);
-    console.log(req.body);
-    res.send('aqui creamos una noticia')
+noticiaCtrl.crearNoticia = async(req, res) => {
+    try {
+        //validar 
+
+        //crear un nuevo objeto para guardar en la DB
+        const noticiaNueva = new Noticia({
+            categoria: req.body.categoria,
+            titulo: req.body.titulo,
+            descripcion: req.body.descripcion,
+            linkImagen: req.body.linkImagen,
+        });
+        // guardar producto en DB
+        await noticiaNueva.save()
+        // enviar la respuesta
+        res.status(201).json({
+            mensaje: 'Producto agregado correctamente'
+        })
+    } catch (error) {
+        console.log(error);
+        // enviar codigo de error
+        res.status(400).json({
+            mensaje: 'Error al agregar un producto'
+        })
+    }
 }
 export default noticiaCtrl
---
+++ src/models/noticia.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from 'mongoose';
+
+const noticiaSchema = new Schema({
+    categoria:{
+        type: String,
+        required:true,
+        maxlength:10,
+    },
+    titulo:{
+        type: String,
+        required:true,
+        maxlength:30,
+    },
+    descripcion:{
+        type: String,
+        required:true,
+        maxlength:3000,
+    },
+    linkImagen:{
+        type: String,
+        required:true,
+        maxlength:1000,
+    }
+})
+
+const Noticia = mongoose.model('noticia', noticiaSchema)
+
+export default Noticia;
