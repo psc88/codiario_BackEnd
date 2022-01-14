048b7beffdd671e237e0b59f0f809617592c2b62
Pablo Castillo <psc88sdrd@gmail.com>
2022-01-14 00:10:04 -0300

    se agregio req.body.nombreComentador

--- src/controllers/comentarios.controllers.js
+++ src/controllers/comentarios.controllers.js
@@ -17,7 +17,7 @@ comentarioCtrl.listarComentario = async(req,res) => {
 comentarioCtrl.crearComentario = async(req, res) => {
     try {
         const comentarioNueva = new Comentario({
-            nombreComentador: req.body.nombre,
+            nombreComentador: req.body.nombreComentador,
             comentario: req.body.comentario
         });
         console.log(comentarioNueva)
